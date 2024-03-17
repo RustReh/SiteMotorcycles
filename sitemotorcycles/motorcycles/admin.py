90fe8adcaf453f08eee4afc275bdcda5a1f3bd35
rustem <hyshikryst@yandex.ru>
2024-03-17 14:26:20 +0400

    add published manager to model, edit admin

--- sitemotorcycles/motorcycles/admin.py
+++ sitemotorcycles/motorcycles/admin.py
@@ -6,15 +6,20 @@ from .models import Motorcycles, KindOfMotorcycle, EngineType
 
 @admin.register(Motorcycles)
 class MotorcyclesAdmin(admin.ModelAdmin):
-    fields = ['photo', 'brand', 'bike_model', 'slug', 'description', 'kind', 'type']
+    fields = ['photo', 'brand', 'bike_model', 'slug', 'description', 'kind', 'type', 'is_published']
+    ordering = ['time_create', 'slug']
+    list_display_links = ('brand', 'bike_model',)
     prepopulated_fields = {"slug": ("brand", 'bike_model' )}
-    list_display = ('photo', 'brand', 'bike_model', 'kind', 'type')
+    list_display = ('photo', 'brand', 'bike_model', 'kind', 'type', 'is_published')
+    list_editable = ('is_published',)
+    list_per_page = 5
 
 
 @admin.register(KindOfMotorcycle)
 class KindAdmin(admin.ModelAdmin):
     list_display = ('id', 'name', 'photo_kind')
     list_display_links = ('id', 'name', 'photo_kind')
+    ordering = ['id']
 
 
 @admin.register(EngineType)
